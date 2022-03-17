db5af6c6e2b31f3f6e1ab97e17031ea9191198fa
Daniel <djc.usa98@gmail.com>
2022-03-16 22:05:34 -0500

    adding information to not forget

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+"""
+Information regarding a unit is under a div with class=mw-parser-output, this div has a different number of children 
+depending on whether a unit is a tranforming/exchange/fusion/etc. unit so it is important to make a distinction
+in order to pull all the information regarding the unit.
+"""
 def tab_check(unit) -> str:
     count = 0
     for x in unit.children:
@@ -13,6 +17,13 @@ def tab_check(unit) -> str:
 
 html_response = requests.get('https://dbz-dokkanbattle.fandom.com/wiki/Boiling_Power_Super_Saiyan_Goku').text
 soup = BeautifulSoup(html_response, 'lxml')
-right = soup.find('div', class_ ='mw-parser-output')
-print(type(right))
-print(tab_check(right))
+all_info = soup.find('div', class_ ='mw-parser-output')
+print(tab_check(all_info))
+
+"""
+ignore first child grab rest for units with multiple info
+"""
+count = 0
+for x in all_info.find('div', class_="tabber wds-tabber").children:
+    count +=1
+print(count)
